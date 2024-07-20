c078369a70bc262d5b3ee7680d966aa4bffd7eac
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-07-20 22:39:52 +0200

    Update JS file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById("registration-form");
     const feedbackDiv = document.getElementById("form-feedback");
 
