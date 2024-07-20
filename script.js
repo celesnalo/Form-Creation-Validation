729c2627942f0d63e73047c8bdd1d0ba5fec7545
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-07-20 22:36:21 +0200

    Added content to JS file

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById("form-feedback");
+
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value.trim();
+
+        let isValid = true;
+        const messages = [];
+
+        if (username.length < 3) {
+            isValid = false;
+            messages.push("Input invalid! Please try again.");
+        }
+
+        if (!(email.includes("@") && email.includes("."))) {
+            isValid = false;
+            messages.push("Invalid email, please enter a valid email address.");
+        }
+
+        if (password.length < 8) {
+            isValid = false;
+            messages.push("Password must contain at least 8 characters.");
+        }
+
+        feedbackDiv.style.display = "block";
+        if (isValid) {
+            feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = "#28a745";
+        } else {
+            feedbackDiv.innerHTML = messages.join("<br>");
+            feedbackDiv.style.color = "#dc3545";
+        }
+    });
+});
